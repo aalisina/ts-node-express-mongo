2439e62413d09872a3828401e73a985830f19ddc
sinazay123 <sinazay123@gmail.com>
2022-10-10 16:42:30 -0500

    Added AnyZodObject as the interface for the schema which accepts any object

--- src/validators/validateResources.ts
+++ src/validators/validateResources.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod";
 // Function returning another function
 // Currying is using a function to return another function
 const validate =
-  (schema) => (req: Request, res: Response, next: NextFunction) => {};
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {};
