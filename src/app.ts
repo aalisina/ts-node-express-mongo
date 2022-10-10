c7028da5c4ad54a4eeac2ef993e8d527d2b0177c
sinazay123 <sinazay123@gmail.com>
2022-10-10 15:53:28 -0500

    Replaced clg's with the new logger function

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import config from "config";
 import dbConnection from "./utils/dbConnection";
+import logger from "./utils/logger";
 
 const PORT = config.get<number>("port");
 const app = express();
 
 app.listen(PORT, async () => {
-  console.log("App esta corriendo");
+  logger.info("App esta corriendo");
   await dbConnection();
 });
--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import config from "config";
+import logger from "./logger";
 
 async function connectDatabase() {
   const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to DB");
+    logger.info("Connected to DB");
   } catch (error) {
-    console.log(error);
+    logger.error(error);
     process.exit(1);
   }
 }
