d498d5bb4df8f50a82d2dd82b2a542877f763d7d
sinazay123 <sinazay123@gmail.com>
2022-10-10 15:43:05 -0500

    Added the function to connect to the db to the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import config from "config";
+import dbConnection from "./utils/dbConnection";
 
 const PORT = config.get<number>("port");
 const app = express();
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("App esta corriendo");
+  await dbConnection();
 });
