fded501e1b0908f79c9ae1bbdc1ff5c8fb99afa6
sinazay123 <sinazay123@gmail.com>
2022-10-10 21:38:49 -0500

    Added a createUserService function

---
+++ src/services/user.service.ts
@@ -0,0 +1,12 @@
+import { DocumentDefinition } from "mongoose";
+import UserModel, { UserDocument } from "../models/user.model";
+
+export async function createUserService(
+  input: DocumentDefinition<UserDocument>
+) {
+  try {
+    return await UserModel.create(input);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
