c945c13dd99a3d9cbb18af82eb63cddda1a28879
sinazay123 <sinazay123@gmail.com>
2022-10-10 22:10:01 -0500

    Added the CreateUserInput interface to the controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import { createUserService } from "../services/user.service";
 import logger from "../utils/logger";
+import { CreateUserInput } from "../validators/user.schema";
 
-export async function createUserHandler(req: Request, res: Response) {
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
   try {
     const user = await createUserService(req.body); // call create user service
   } catch (err: any) {
