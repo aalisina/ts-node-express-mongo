cdf7d615879168b5bb8d02c8669424bec292d3f1
sinazay123 <sinazay123@gmail.com>
2022-10-10 21:41:12 -0500

    Added the createUserService to the controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { createUserService } from "../services/user.service";
 import logger from "../utils/logger";
-export function createUserHandler(req: Request, res: Response) {
+
+export async function createUserHandler(req: Request, res: Response) {
   try {
-    //const user = await; // call create user service
+    const user = await createUserService(req.body); // call create user service
   } catch (err: any) {
     logger.error(err);
     res.status(409).send(err.message);
