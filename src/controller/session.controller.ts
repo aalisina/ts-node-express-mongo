6164ae392cb29be021245165b6553be97babf868
sinazay123 <sinazay123@gmail.com>
2022-10-30 17:56:45 -0500

    Added a session controller with the steps to take next

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+  // Validate the user's pwd
+  // create a session
+  // create an access token
+  // create a refresh token
+  // return access and refresh tokens
+}
