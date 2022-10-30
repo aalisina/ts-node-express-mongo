c862036889568b28f5edbacdcf3e49c6ee909343
sinazay123 <sinazay123@gmail.com>
2022-10-30 18:31:30 -0500

    Added a function to sign a jwt

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,6 +1,14 @@
 import jwt from "jsonwebtoken";
 import config from "config";
 
-function signJwt() {}
+const privateKey = config.get<string>("privateKey");
+const publicKey = config.get<string>("publicKey");
+
+function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  jwt.sign(object, privateKey, {
+    ...options,
+    algorithm: "RS256",
+  });
+}
 
 function verifyJwt() {}
