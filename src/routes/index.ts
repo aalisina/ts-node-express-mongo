d2967f779321b3f3fa90c58250f8b72d62d02dea
sinazay123 <sinazay123@gmail.com>
2022-11-01 19:48:01 -0500

    Added a session schema and route for creating a session

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Express, Request, Response } from "express";
+import { createUserSessionHandler } from "../controller/session.controller";
 import { createUserHandler } from "../controller/user.controller";
+import { createSessionSchema } from "../validators/session.schema";
 import { createUserSchema } from "../validators/user.schema";
 import validateResource from "../validators/validateResources";
 
@@ -9,5 +11,11 @@ function routes(app: Express) {
   });
 
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 export default routes;
