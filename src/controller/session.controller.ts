8bcb71b3d429f7a7adfe598f9a19c4ca72d35216
sinazay123 <sinazay123@gmail.com>
2022-11-01 19:30:53 -0500

    Added function to create an access token to the controller

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
+import config from "config";
 import { Request, Response } from "express";
 import { createSession } from "../services/session.service";
 import { validatePassword } from "../services/validatePassword";
+import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate the user's pwd
@@ -9,8 +11,12 @@ export async function createUserSessionHandler(req: Request, res: Response) {
     return res.status(401).send("Invalid email or password.");
   }
   // create a session
-  const session = createSession(user._id, req.get("user-agent") || "");
+  const session = await createSession(user._id, req.get("user-agent") || "");
   // create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTtl") } // 15 mins
+  );
   // create a refresh token
   // return access and refresh tokens
 }
