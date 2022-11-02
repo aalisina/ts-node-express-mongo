a1d7baeaad3b33760ce9f9e0928d82e3bd04b6d6
sinazay123 <sinazay123@gmail.com>
2022-11-02 10:01:58 -0500

    Added a handler to delete a session

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -32,3 +32,11 @@ export async function getUserSessionsHandler(req: Request, res: Response) {
 
   return res.send(sessions);
 }
+export async function deleteSessionHandler(req: Request, res: Response) {
+  const sessionId = res.locals.user.session;
+
+  return res.send({
+    accessToken: null,
+    refreshToken: null,
+  });
+}
