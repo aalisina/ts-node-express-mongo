0809bfe60e702b0feb76504930d67e541c7f5075
sinazay123 <sinazay123@gmail.com>
2022-11-02 10:09:01 -0500

    Added the query and update parameters to the controller

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,10 @@
 import config from "config";
 import { Request, Response } from "express";
-import { createSession, findSessions } from "../services/session.service";
+import {
+  createSession,
+  findSessions,
+  updateSession,
+} from "../services/session.service";
 import { validatePassword } from "../services/validatePassword";
 import { signJwt } from "../utils/jwt.utils";
 
@@ -34,6 +38,8 @@ export async function getUserSessionsHandler(req: Request, res: Response) {
 }
 export async function deleteSessionHandler(req: Request, res: Response) {
   const sessionId = res.locals.user.session;
+
+  await updateSession({ _id: sessionId }, { valid: false });
 
   return res.send({
     accessToken: null,
