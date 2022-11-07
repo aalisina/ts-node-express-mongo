13a00a42ddb6f716654b5d8e0ce01dae7cf4058f
sinazay123 <sinazay123@gmail.com>
2022-11-07 18:56:48 -0500

    Added the routes for the crud of products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,23 @@
 import { Express, Request, Response } from "express";
 import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "../controller/product.controller";
+import {
   createUserSessionHandler,
   deleteSessionHandler,
   getUserSessionsHandler,
 } from "../controller/session.controller";
 import { createUserHandler } from "../controller/user.controller";
 import { requireUser } from "../middlewares/requireUser";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "../validators/product.schema";
 import { createSessionSchema } from "../validators/session.schema";
 import { createUserSchema } from "../validators/user.schema";
 import validateResource from "../validators/validateResources";
@@ -25,5 +37,27 @@ function routes(app: Express) {
 
   app.get("/api/sessions", requireUser, getUserSessionsHandler);
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+  // product routes
+  app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+  app.put(
+    "/api/products",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+  app.get(
+    "/api/products",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+  app.delete(
+    "/api/products",
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 export default routes;
